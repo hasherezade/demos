cef80e9f599517b77804a4fe6455839e79484227
hasherezade <hasherezade@op.pl>
2016-08-25 21:28:16 +0200

    [ADD][functions_loader] Init

---
+++ functions_loader/src/main.cpp
@@ -0,0 +1,60 @@
+#include <windows.h>
+#include <stdio.h>
+#include "ntddk.h"
+#include "peb_lookup.h"
+#include "exports_lookup.h"
+
+
+//tries to fetch module base via PEB,
+//compares the result with output of analogical function GetModuleHandleW
+LPVOID test_fetching_module(LPWSTR libName)
+{
+    LPVOID base = get_module_base(libName);
+    if (base == GetModuleHandleW(libName)) {
+        printf("[OK] %S : %p\n", libName, base);
+        return base;
+    }
+    printf("[FAILED] %S\n", libName);
+    return NULL;
+}
+
+//tries to fetch module base via export table,
+//compares the result with output of analogical function GetProcAddress
+LPVOID test_fetching_func(HMODULE hModule, LPSTR funcName)
+{
+    LPVOID hFunc = get_exported_func(hModule, funcName);
+    if (hFunc == GetProcAddress((HMODULE)hModule, funcName)) {
+        printf("[OK] %s : %p\n", funcName, hFunc);
+        return hFunc;
+    }
+    printf("[FAILED] %s\n", funcName);
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    BYTE* res_data = NULL;
+    SIZE_T res_size = 0;
+
+    LPVOID base = test_fetching_module(L"kernel32.dll");
+    if (!base) {
+        system("pause");
+        return -1;
+    }
+
+    if (!test_fetching_func((HMODULE) base, "LoadLibraryA")) {
+        system("pause");
+        return -1;
+    }
+    if (!test_fetching_func((HMODULE) base, "GetProcAddress")) {
+        system("pause");
+        return -1;
+    }
+    if (!test_fetching_func((HMODULE) base, "NeedCurrentDirectoryForExePathW")) {
+        system("pause");
+        return -1;
+    }
+    printf("All tests passed!\n");
+    system("pause");
+    return 0;
+}
---
+++ functions_loader/src/pe_hdrs_helper.cpp
@@ -0,0 +1,29 @@
+#include "pe_hdrs_helper.h"
+
+IMAGE_NT_HEADERS* get_nt_hrds(BYTE *pe_buffer)
+{
+    if (pe_buffer == NULL) return NULL;
+
+    IMAGE_DOS_HEADER *idh = NULL;
+    IMAGE_NT_HEADERS *inh = NULL;
+
+    idh = (IMAGE_DOS_HEADER*)pe_buffer;
+    if (idh->e_magic != IMAGE_DOS_SIGNATURE) return NULL;
+    inh = (IMAGE_NT_HEADERS *)((BYTE*)pe_buffer + idh->e_lfanew);
+    return inh;
+}
+
+IMAGE_DATA_DIRECTORY* get_pe_directory(PVOID pe_buffer, DWORD dir_id)
+{
+    if (dir_id > 16) return NULL;
+
+    //fetch relocation table from current image:
+    PIMAGE_NT_HEADERS nt_headers = get_nt_hrds((BYTE*) pe_buffer);
+    if (nt_headers == NULL) return NULL;
+
+    IMAGE_DATA_DIRECTORY* peDir = &(nt_headers->OptionalHeader.DataDirectory[dir_id]);
+    if (peDir->VirtualAddress == NULL) {
+        return NULL;
+    }
+    return peDir;
+}
