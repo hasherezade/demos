3ac13dd09e43444cb1b009a704e1c610e29a98fd
hasherezade <hasherezade@op.pl>
2016-11-03 14:37:42 +0100

    [REFACT][FunctionsLoader] Refactored to compile on 64 bit system

--- functions_loader/src/main.cpp
+++ functions_loader/src/main.cpp
@@ -9,11 +9,18 @@
 
 int main(int argc, char *argv[])
 {
+#if defined(_WIN64)
+    // 64 bit not supported! Compile this program as a 32bit application!
+    printf("64 bit not supported! Compile this program as a 32bit application!\n");
+    system("pause");
+    return -1;
+#else
     if (!test_loading()) {
         system("pause");
         return -1;
     }
     load_and_popup();
+#endif
     system("pause");
     return 0;
 }
